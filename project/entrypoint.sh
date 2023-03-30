a19b8c4270ab535cd2057882280fad3ea4a8e86f
Lior Rabanian <masrab11@gmail.com>
2023-03-30 14:30:16 +0300

    fully worked

--- project/entrypoint.sh
+++ project/entrypoint.sh
@@ -1,9 +1,21 @@
-#!/bin/bash
+#!/bin/sh
 
-./manage.py migrate
+CONTAINER_FIRST_STARTUP="CONTAINER_FIRST_STARTUP"
+if [ ! -e /$CONTAINER_FIRST_STARTUP ]; then
+    touch /$CONTAINER_FIRST_STARTUP
+    ./manage.py migrate
 
-./manage.py updatecatalog
+    ./manage.py updatecatalog
 
-./manage.py collectstatic
+    ./manage.py collectstatic
+else
+    echo
+fi
+
+# ./manage.py migrate
+
+# ./manage.py updatecatalog
+
+# ./manage.py collectstatic
 
 ./manage.py runserver ${BIND_HOST}:${BIND_PORT}
